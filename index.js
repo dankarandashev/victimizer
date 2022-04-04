4b981eb5d2806b00d33c0233d682170914352f42
dankarandashev <dan.karandashev@gmail.com>
2022-04-04 13:07:16 +0300

    add possibility to add many items

--- index.js
+++ index.js
@@ -1,22 +1,119 @@
 let data = [
-  { id: 0, name: "Axel", isVictim: false, isShown: true },
-  { id: 1, name: "Denis", isVictim: false, isShown: true },
-  { id: 2, name: "Anastasiia", isVictim: false, isShown: true },
-  { id: 3, name: "Elena", isVictim: false, isShown: true },
-  { id: 4, name: "Dustin", isVictim: false, isShown: true },
-  { id: 5, name: "Fritz", isVictim: false, isShown: true },
-  { id: 6, name: "Irina", isVictim: false, isShown: true },
-  { id: 7, name: "Jerome", isVictim: false, isShown: true },
-  { id: 8, name: "Lena", isVictim: false, isShown: true },
-  { id: 9, name: "Olga", isVictim: false, isShown: true },
-  { id: 10, name: "Ronny", isVictim: false, isShown: true },
-  { id: 11, name: "Sergei", isVictim: false, isShown: true },
-  { id: 12, name: "Valeria", isVictim: false, isShown: true },
-  { id: 13, name: "Viktoriia", isVictim: false, isShown: true },
+  // { id: 0, name: "Axel", isVictim: false, isShown: true },
+  // { id: 1, name: "Denis", isVictim: false, isShown: true },
+  // { id: 2, name: "Anastasiia", isVictim: false, isShown: true },
+  // { id: 3, name: "Elena", isVictim: false, isShown: true },
+  // { id: 4, name: "Dustin", isVictim: false, isShown: true },
+  // { id: 5, name: "Fritz", isVictim: false, isShown: true },
+  // { id: 6, name: "Irina", isVictim: false, isShown: true },
+  // { id: 7, name: "Jerome", isVictim: false, isShown: true },
+  // { id: 8, name: "Lena", isVictim: false, isShown: true },
+  // { id: 9, name: "Olga", isVictim: false, isShown: true },
+  // { id: 10, name: "Ronny", isVictim: false, isShown: true },
+  // { id: 11, name: "Sergei", isVictim: false, isShown: true },
+  // { id: 12, name: "Valeria", isVictim: false, isShown: true },
+  // { id: 13, name: "Viktoriia", isVictim: false, isShown: true },
 ];
 
 let counter = 0;
 let previousVictimId = -1;
+let useEnter = false;
+
+const ENTER_ON = 'Enter (on)';
+const ENTER_OFF = 'Enter (off)';
+const HINT = `
+${"The list is empty."}<br>
+${"Please add items using the form below."}<br>
+${'The items should be separated by " " (spaсe) and/or ";" (semicolon)'}`;
+
+const addOne = document.getElementById("addMany");
+addOne.addEventListener("click", () => {
+  addUsers();
+});
+
+function addUsers() {
+  let input = document.getElementById("addManyInput").innerText.trim();
+  addStringToData(input);
+  document.getElementById("addManyInput").innerText = "";
+  showUsers();
+}
+
+function checkIfListIsEmpty() {
+  if (!data.length) {
+    document.getElementById("hint").innerHTML = HINT;
+  } else {
+    document.getElementById("hint").innerHTML = "";
+  }
+}
+
+function addStringToData(input) {
+  const array = input.replace(/\s/g, ";").split(";");
+  array.forEach((element) => {
+    if (element.length > 0) {
+      const id = data.length;
+      let newUserName =
+        element.charAt(0).toUpperCase() + element.slice(1).toLowerCase();
+      let sameNameNumber = 1;
+      data.forEach((el) => {
+        el.name == newUserName ? sameNameNumber++ : "";
+      });
+      if (sameNameNumber > 1) {
+        newUserName = newUserName + "_" + sameNameNumber;
+      }
+      data.push({
+        id: id,
+        name: newUserName,
+        isVictim: false,
+        isShown: true,
+      });
+    }
+  });
+}
+
+function reassigneIds() {
+  for (let i = 0; i < data.length; i++) {
+    const user = data[i];
+    user.id = i;
+  }
+}
+
+function markAsNoVictimsAndShown() {
+  for (let i = 0; i < data.length; i++) {
+    const user = data[i];
+    user.isVictim = false;
+    user.isShown = true;
+  }
+}
+
+function useEnterToggle() {
+  useEnter = !useEnter;
+  const useEnterToggleBtn = document.getElementById("useEnterToggle");
+  const input = document.getElementById("addManyInput");
+  enterOnOffTextToggle(useEnterToggleBtn);
+  useEnterToggleBtn.addEventListener("click", () => {
+    useEnter = !useEnter;
+    if (useEnter) {
+      input.addEventListener("keyup", addEventListenerToInput);
+      useEnterToggleBtn.innerText = ENTER_ON;
+      console.log('use enter = true');
+    } else {
+      input.removeEventListener("keyup", addEventListenerToInput);
+      useEnterToggleBtn.innerText = ENTER_OFF;
+      console.log('use enter = false');
+    }
+  });
+}
+
+function addEventListenerToInput(event) {
+  if (event.key == "Enter") {
+    addUsers();
+  }
+}
+
+function enterOnOffTextToggle(useEnterToggleBtn) {
+  console.log('here')
+  useEnterToggleBtn.innerText = useEnter === true ?  ENTER_ON : ENTER_OFF;
+}
 
 function chooseVictim() {
   if (counter === data.length - 1) {
@@ -24,11 +121,17 @@ function chooseVictim() {
   }
   if (counter === data.length) {
     counter = 0;
-    location.reload();
+    previousVictimId = -1;
+    const div = document.getElementById("victimsList");
+    const btn = document.getElementById("button");
+    markAsNoVictimsAndShown();
+    div.innerHTML = "";
+    btn.innerHTML = "Press to choose a victim!";
+    showUsers();
     return;
   }
   const user = findRandomUser();
-  if ((user.isVictim && counter < data.length) || user.isShown === false) {
+  if (user.isVictim && counter < data.length) {
     chooseVictim();
     return;
   }
@@ -65,25 +168,24 @@ function findUserInListById(id) {
 
 function showUsers() {
   showCounter();
+  // showDate();
+  checkIfListIsEmpty();
+  // useEnterToggle();
 
-  if (counter === data.length) {
-    location.reload();
-    return;
-  }
   const div = document.getElementById("victimsList");
   div.innerHTML = "";
   const ul = document.createElement("ul");
   for (const id in data) {
     if (id) {
       const user = findUserInListById(id);
-      if (user && user.isShown) {
+      if (user) {
         const li = document.createElement("li");
-        // li.addEventListener("click", () => {
-        //   deleteUser(user);
-        // });
+        li.addEventListener("click", () => {
+          deleteUser(user);
+        });
         li.innerHTML = user.name;
         li.setAttribute("id", id);
-        // li.innerHTML = "<button>х</button> &nbsp;" + user.name;
+        // li.innerHTML = "<button class='deleteBtn'>X</button> &nbsp;" + user.name;
         li.innerHTML = user.name;
         ul.append(li);
       }
@@ -92,20 +194,27 @@ function showUsers() {
   div.append(ul);
 }
 
+// function showDate() {
+//   const today = new Date();
+//   const d = `${today.getFullYear()} / ${
+//     today.getMonth() + 1
+//   } / ${today.getDate()}`;
+//   document.getElementById("date").innerHTML = d;
+// }
+
 function showCounter() {
   const ourCounter = document.getElementById("counter");
   const dataLength = data.length;
   let result = `${counter}/${dataLength}`;
   ourCounter.innerHTML = result;
-  console.log(counter);
 }
 
 function deleteUser(user) {
-  user.isShown = false;
-  counter++;
-  if (counter === data.length) {
-    document.getElementById("button").innerHTML = "Refresh";
-  }
+  data = data.filter((u) => u.id != user.id);
+  counter = 0;
+  previousVictimId = -1;
+  markAsNoVictimsAndShown();
+  reassigneIds();
   showUsers();
 }
 
